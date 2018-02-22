8bcfd96028fdc2836e27f30ed245f464a103c996
EvgeniyBakunovich <bakunovichevgeniy@gmail.com>
2018-02-22 22:25:37 +0300

    Class names and img fix

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,4 @@
 var loadFile = function(event){
-	var output = document.getElementById('pic');
+	var output = document.getElementsByClassName('pic')[0];
 	output.src = URL.createObjectURL(event.target.files[0]);
 };
