9b1d02febd17f1b8b41c8ffa0f69e573593fa141
MaximSkavysh <maximgen666@gmail.com>
2018-02-24 21:21:33 +0300

    add check

--- assets/js/dataReader.js
+++ assets/js/dataReader.js
@@ -1,22 +1,28 @@
 $(document).ready(function () {
-    $.getJSON('assets/js/data.json', function (data) {
-        var vacanciesData = '';
-        $.each(data, function (key, value) {
-            vacanciesData += '<tr>';
-            vacanciesData += '<td>' + value.vacancy + '</td>';
-            vacanciesData += '<td>' + value.skill + '</td>';
-            vacanciesData += '<td>' + value.pay + '</td>';
-            vacanciesData += '<td>' + value.fullname + '</td>';
-            vacanciesData += '</tr>';
+        $.getJSON('assets/js/data.json')
+            .done(function (data) {
+                var vacanciesData = '';
+                $.each(data, function (key, value) {
+                    vacanciesData += '<tr>';
+                    vacanciesData += '<td>' + value.vacancy + '</td>';
+                    vacanciesData += '<td>' + value.skill + '</td>';
+                    vacanciesData += '<td>' + value.pay + '</td>';
+                    vacanciesData += '<td>' + value.fullname + '</td>';
+                    vacanciesData += '</tr>';
+                });
+                $("#vacancies-table").append(vacanciesData)
+            })
+            .fail(function (jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                console.log("Request Failed: " + err);
+            });
+        $("#searchInput").on("keyup", function () {
+            var value = $(this).val().toLowerCase();
+            $("table tr").filter(function (index) {
+                if (index > 0) {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                }
+            });
         });
-        $("#vacancies-table").append(vacanciesData);
-    });
-    $("#searchInput").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("table tr").filter(function (index) {
-            if (index > 0) {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            }
-        });
-    });
-});
+    }
+);
