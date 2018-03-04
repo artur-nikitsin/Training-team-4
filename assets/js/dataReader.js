6a767ea38956f5232e86aa73abc9dc593d0a57ac
Artur Nikitsin <artur.nikitsin@gmail.com>
2018-03-04 14:29:15 +0300

    united tasks 10-13, added datatable plugin

--- assets/js/dataReader.js
+++ assets/js/dataReader.js
@@ -10,19 +10,48 @@ $(document).ready(function () {
                     vacanciesData += "<td>" + value.fullname + "</td>";
                     vacanciesData += "</tr>";
                 });
-                $("#vacancies-table").append(vacanciesData)
+                $("#vacancies-table tbody").append(vacanciesData)
+
+
+                var table = $('#vacancies-table').DataTable(
+                    {
+                        "searching": false,
+                        "lengthMenu": [10, 20, 50],
+                        "info": false
+                    }
+                );
+
+                // table.on( 'draw', function () {
+                //     console.log( 'Redraw occurred at: '+new Date().getTime() );
+                // } );
+
+
+                // $('#vacancies-table').DataTable(
+                //     {
+                //         "pagingType": "simple_numbers",
+                //         "searching": false,
+                //         "scrollY": true,
+                //         "scrollY": "600px",
+                //         "info": false,
+                //         "lengthMenu": [ 10, 20, 50 ],
+                //         "responsive": true
+                //
+                //     }
+                // );
+
+
             })
             .fail(function (jqxhr, textStatus, error) {
                 var err = textStatus + ", " + error;
                 console.log("Request Failed: " + err);
             });
-        $("#search-input").on("keyup", function () {
-            var value = $(this).val().toLowerCase();
-            $("#vacancies-table tr").filter(function (index) {
-                if (index > 0) {
-                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-                }
-            });
-        });
+        // $("#search-input").on("keyup", function () {
+        //     var value = $(this).val().toLowerCase();
+        //     $("#vacancies-table tr").filter(function (index) {
+        //         if (index > 0) {
+        //             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        //         }
+        //     });
+        // });
     }
 );
