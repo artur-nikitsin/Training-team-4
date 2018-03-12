091de685f485b136f19f457ff024ae1d90347d3b
Vaires <36207924+Vaires@users.noreply.github.com>
2018-03-12 20:26:18 +0300

    Check bugs #20
    Trying to use jquery.

--- assets/js/searchCandidatesScript.js
+++ assets/js/searchCandidatesScript.js
@@ -1,22 +1,28 @@
 $(document).ready(function () {
-        $("#search-input").keyup(function () {
-            var filter, search, person, status, name;
-            search = document.getElementById("search-input").value.toUpperCase();
-            filter = $(".page-candidates-filter-candidates-input option:selected").html();
-            person = $(".page-candidates-mini-profile");
-            for (i = 0; i < person.length; i++) {
-                status = person[i].getElementsByClassName("mini-profile-stage-interview")[0];
-                name = person[i].getElementsByClassName("mini-profile-info-name-candidate")[0];
-                if (status.innerHTML.indexOf(filter) > -1 && name.innerHTML.toUpperCase().indexOf(search) > -1) {
-                    person[i].style.display = "";
-                } else {
-                    person[i].style.display = "none";
-                }
-                if (search == "") {
-                    person[i].style.display = "";
-                }
-            }
-        });
+    var $search, $filter, $persons;
+
+    function Filters() {
+        search = $("#search-input").val().toUpperCase();
+        filter = $(".page-candidates-filter-candidates-input option:selected").html();
+        $persons = $(".page-candidates-mini-profile");
     }
-);
+
+    $("#search-input").keyup(function () {
+        Filters();
+        var status, name;
+        for (var i = 0; i < $persons.length; i++) {
+            status = $persons[i].getElementsByClassName("mini-profile-stage-interview")[0];
+            name = $persons[i].getElementsByClassName("mini-profile-info-name-candidate")[0];
+            if (status.innerHTML.indexOf(filter) > -1 && name.innerHTML.toUpperCase().indexOf(search) > -1) {
+                $persons.eq(i).show();
+            } else {
+                $persons.eq(i).hide();
+            }
+            if (search == "") {
+                $persons.eq(i).show();
+            }
+        }
+    });
+});
+
 
