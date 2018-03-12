b148563c6c5dd304f9150254061e0d42a848c038
Vaires <36207924+Vaires@users.noreply.github.com>
2018-03-12 21:48:31 +0300

    Update searchCandidatesScript.js

--- assets/js/searchCandidatesScript.js
+++ assets/js/searchCandidatesScript.js
@@ -1,24 +1,21 @@
 $(document).ready(function () {
-    var $search, $filter, $persons;
-
-    function Filters() {
-        search = $("#search-input").val().toUpperCase();
-        filter = $(".page-candidates-filter-candidates-input option:selected").html();
+    var $search, $filter, $persons, $status,$name ;
+    function filters() {
+        $search = $("#search-input").val().toUpperCase();
+        $filter = $(".page-candidates-filter-candidates-input option:selected").html();
         $persons = $(".page-candidates-mini-profile");
     }
-
     $("#search-input").keyup(function () {
-        Filters();
-        var status, name;
+        filters();
         for (var i = 0; i < $persons.length; i++) {
-            status = $persons[i].getElementsByClassName("mini-profile-stage-interview")[0];
-            name = $persons[i].getElementsByClassName("mini-profile-info-name-candidate")[0];
-            if (status.innerHTML.indexOf(filter) > -1 && name.innerHTML.toUpperCase().indexOf(search) > -1) {
+            $status = $persons.eq(i).find(".mini-profile-stage-interview").html();
+            $name = $persons.eq(i).find(".mini-profile-info-name-candidate").html();
+            if ($status.indexOf($filter) > -1 && $name.toUpperCase().indexOf($search) > -1) {
                 $persons.eq(i).show();
             } else {
                 $persons.eq(i).hide();
             }
-            if (search == "") {
+            if ($search == "") {
                 $persons.eq(i).show();
             }
         }
