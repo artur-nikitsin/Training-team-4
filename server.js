449b5e8200ecceb975ef169f206c87c74a416d12
MaximSkavysh <maximgen666@gmail.com>
2018-03-24 19:10:49 +0300

    changes for update DB

--- server.js
+++ server.js
@@ -23,45 +23,49 @@ server.get('/main', function (req, res, next) {
 });
 
 //rest api to get all results
-server.get('/vacancies', function (req, res) {
+server.get('/vacancies', function (req, res, next) {
     connection.query('select * from vacancies', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results));
     });
+    next();
 });
 
 //rest api to get a single vacancies data
 server.get('/vacancies/:id', function (req, res) {
-    connection.query('select * from vacancies where id=?', [req.params.id], function (error, results, fields) {
+    connection.query('select * from vacancies where idVacancies=?', [req.params.id], function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results));
     });
 });
 
 //rest api to create a new record into mysql database
-server.post('/vacancies', function (req, res) {
+server.post('/vacancies', function (req, res, next) {
     var postData = req.body;
     connection.query('INSERT INTO vacancies SET ?', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results));
     });
+    next();
 });
 
 //rest api to update record into mysql database
-server.put('/vacancies', function (req, res) {
-    connection.query('UPDATE `vacancies` SET `vacancies_name`=?,`pay`=?,`skill`=?,`candidate`=? where `Id`=?',
-        [req.body.vacancies_name, req.body.pay, req.body.skill, req.body.candidate, req.body.id], function (error, results, fields) {
+server.put('/vacancies', function (req, res, next) {
+    connection.query('UPDATE `vacancies` SET `Vacancy`=?,`ExperienceLevel`=?,`SalaryEstimate`=?,`JobType`=?,`ReviewCandidates`=? where `idVacancies`=?',
+        [req.body.Vacancy, req.body.ExperienceLevel, req.body.SalaryEstimate, req.body.JobType, req.body.ReviewCandidates, req.body.idVacancies,], function (error, results, fields) {
             if (error) throw error;
             res.end(JSON.stringify(results));
         });
+    next();
 });
 
 //rest api to delete record from mysql database
-server.del('/vacancies/:id', function (req, res) {
-    connection.query('DELETE FROM `vacancies` WHERE `id`=?', [req.params.id], function (error, results, fields) {
+server.del('/vacancies/:id', function (req, res, next) {
+    connection.query('DELETE FROM `vacancies` WHERE `idVacancies`=?', [req.params.id], function (error, results, fields) {
         if (error) throw error;
         res.end('Record has been deleted!');
     });
+    next();
 });
 
 server.listen(8070, function () {
@@ -71,7 +75,6 @@ server.listen(8070, function () {
 server.get('/\/.*/', restify.plugins.serveStatic({
         directory: __dirname + "/public/",
         default: './HR_APP_Page_Mobile.html'
-
     })
 );
 
