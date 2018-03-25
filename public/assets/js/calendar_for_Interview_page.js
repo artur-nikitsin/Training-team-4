b00e9b7f482c1030d27cbde0d37287d1b4c08f14
Artur Nikitsin <artur.nikitsin@gmail.com>
2018-03-25 22:41:01 +0300

    added integration with google calendar api

--- public/assets/js/calendar_for_Interview_page.js
+++ public/assets/js/calendar_for_Interview_page.js
@@ -2,12 +2,31 @@ $('document').ready(function () {
 
 
     $('#calendar-for-interviews').fullCalendar({
+
+        googleCalendarApiKey: 'AIzaSyDmiZJ4bB-b9v2oGEdWjHzmCyDeLZZeTBg',
+        events: {
+            googleCalendarId: 'training.team.mifort@gmail.com'
+        },
+
+        eventClick: function(calEvent, jsEvent, view) {
+
+            alert('Event: ' + calEvent.title);
+            alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+            alert('View: ' + view.name);
+
+            // change the border color just for fun
+            $(this).css('border-color', 'red');
+
+        },
+
+
+
         themeSystem: 'bootstrap3',
         customButtons: {
             schedule: {
                 text: 'schedule',
                 click: function() {
-                    alert('clicked the custom button!');
+
                 }
             }
         },
