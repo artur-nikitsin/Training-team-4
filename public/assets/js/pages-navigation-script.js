54d1650364a8b7c90448341085716c7eae217fb2
Artur Nikitsin <artur.nikitsin@gmail.com>
2018-03-25 19:41:53 +0300

    on click location to one candidate page in page candidates - changet pages-navigation-script

--- public/assets/js/pages-navigation-script.js
+++ public/assets/js/pages-navigation-script.js
@@ -1,51 +1,18 @@
-// /*TODO: name that file the same style as all the rest are named*/
-// $(document).ready(function () {
-//
-//
-//     $('.page-vacancies').hide();
-//     $('.page-candidates').hide();
-//     $('.one-candidate-information').hide();
-//
-//
-//     $("#navbar-menu ").find("li div").on("click", function () {
-//
-//         var a = this.getAttribute("id")
-//
-//         if (a === "page-vacancies") {
-//
-//             $('.page-vacancies').show();
-//             $('.page-candidates').hide();
-//             $('.one-candidate-information').hide();
-//             $('.button-edit-user-data').hide();
-//             $('.button-add-candidat').hide();
-//         }
-//
-//         if (a === "page-candidates") {
-//
-//             $('.page-candidates').show();
-//             $('.page-vacancies').hide();
-//             $('.one-candidate-information').hide();
-//             $('.button-edit-user-data').hide();
-//             $('.button-add-vacancy').hide();
-//             $('.button-add-candidat').show();
-//
-//             $(".page-candidates-mini-profile").on("click", function () {
-//
-//
-//                 $('.page-candidates').hide();
-//                 $('.page-vacancies').hide();
-//                 $('.one-candidate-information').show();
-//                 $('.button-edit-user-data').show();
-//                 $('.button-add-vacancy').hide();
-//                 $('.button-add-candidat').hide();
-//
-//             });
-//
-//
-//         }
-//
-//
-//     })
-//
-//
-// });
+/*TODO: name that file the same style as all the rest are named*/
+$(document).ready(function () {
+
+
+
+
+            $(".page-candidates-mini-profile").on("click", function () {
+
+
+                window.location.href="HR_APP_Page_Mobile_One_Candidate.html"
+
+            });
+
+
+
+
+
+});
