3386644dcdef4a5ccd12512c02b2966075bfe330
MaximSkavysh <maximgen666@gmail.com>
2018-03-30 22:57:05 +0300

    change for db

--- server.js
+++ server.js
@@ -3,6 +3,32 @@ const config = require('./app/config/config'),
     mysql = require('mysql'),
 fs = require('fs');
 
+
+// class Database {
+//     constructor( config ) {
+//         this.connection = mysql.createConnection( config );
+//     }
+//     query( sql, args ) {
+//         return new Promise( ( resolve, reject ) => {
+//             this.connection.query( sql, args, ( err, rows ) => {
+//                 if ( err )
+//                     return reject( err );
+//                 resolve( rows );
+//             } );
+//         } );
+//     }
+//     close() {
+//         return new Promise( ( resolve, reject ) => {
+//             this.connection.end( err => {
+//                 if ( err )
+//                     return reject( err );
+//                 resolve();
+//             } );
+//         } );
+//     }
+// }
+
+
 var connection = config.db.get;
 /**
  * Initialize Server
